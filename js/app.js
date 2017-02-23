58a8b25182e692a00a744aeddf272f1e220d8d95
chenjenping <chen.jen.ping@hotmail.com>
2017-02-24 02:33:09 +0800

    jquery diary

---
+++ js/app.js
@@ -0,0 +1,99 @@
+$(function () {
+    'use strict';
+
+    var ENTER_KEY = 13;
+    var ESCAPE_KEY = 27;
+
+    var util = {
+        initial_data: {
+            theme: 'default',
+            entries: []
+        },
+        formatTime: function (date) {
+            var weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+            var year = date.getFullYear();
+            var month = date.getMonth() + 1;
+            var day = date.getDate();
+            var weekday = date.getDay();
+            var hours = date.getHours();
+            var minutes = date.getMinutes();
+            if (month < 10) {
+                month = '0' + month;
+            }
+            if (day < 10) {
+                day = '0' + day
+            }
+            if (hours < 10) {
+                hours = '0' + hours;
+            }
+            if (minutes < 10) {
+                minutes = '0' + minutes;
+            }
+            return hours + ':' + minutes + ', ' + weekdays[weekday] + ', ' + year + '/' + month + '/' + day;
+        },
+        store: function (namespace, data) {
+            if (data === undefined) {
+                var store = localStorage.getItem(namespace);
+                return (store && JSON.parse(store)) || this.initial_data;
+            } else {
+                localStorage.setItem(namespace, JSON.stringify(data));
+            }
+        }
+    };
+
+    var app = {
+        init: function () {
+            this.data = util.store('jquery_diary');
+            this.entriesTemplate = window.entries_template;
+            this.bindEvents();
+            this.setTheme(this.data.theme);
+            this.render();
+        },
+        bindEvents: function () {
+            $('#new_entry').on('keydown', this.preventDefault.bind(this));
+            $('#new_entry').on('keyup', this.create.bind(this));
+            $('#theme').on('change', this.selectTheme.bind(this));
+        },
+        setTheme: function (theme) {
+            this.data.theme = theme;
+            util.store('jquery_diary', this.data);
+            $('body').removeClass().addClass(theme);
+            $('#theme').val(theme);
+        },
+        selectTheme: function (e) {
+            var theme = $(e.target).val();
+            this.setTheme(theme);
+        },
+        render: function () {
+            util.store('jquery_diary', this.data);
+            var entries = this.data.entries.sort(function(a, b) {
+                return (a.created_at < b.created_at) ? 1 : -1;
+            });
+            $('#diary').html(this.entriesTemplate({entries: entries, formatTime: util.formatTime}));
+            $('#new_entry').focus();
+        },
+        preventDefault: function (e) {
+            if (e.which === ENTER_KEY && !e.shiftKey) {
+                e.preventDefault();
+            }
+            return true;
+        },
+        create: function (e) {
+            var $input = $(e.target);
+            var content = $input.val().trim();
+
+            if (e.which !== ENTER_KEY || e.shiftKey || !content) {
+                return false;
+            }
+
+            this.data.entries.push({
+                content: content,
+                created_at: (new Date()).toJSON()
+            });
+            $input.val('');
+            this.render();
+        }
+    };
+
+    app.init();
+});
---
+++ template/dist/entries.js
@@ -0,0 +1,19 @@
+function pug_escape(e){var a=""+e,t=pug_match_html.exec(a);if(!t)return e;var r,c,n,s="";for(r=t.index,c=0;r<a.length;r++){switch(a.charCodeAt(r)){case 34:n="&quot;";break;case 38:n="&amp;";break;case 60:n="&lt;";break;case 62:n="&gt;";break;default:continue}c!==r&&(s+=a.substring(c,r)),c=r+1,s+=n}return c!==r?s+a.substring(c,r):s}
+var pug_match_html=/["&<>]/;function entries_template(locals) {var pug_html = "", pug_mixins = {}, pug_interp;;var locals_for_with = (locals || {});(function (Date, entries, formatTime) {// iterate entries
+;(function(){
+  var $$obj = entries;
+  if ('number' == typeof $$obj.length) {
+      for (var pug_index0 = 0, $$l = $$obj.length; pug_index0 < $$l; pug_index0++) {
+        var entry = $$obj[pug_index0];
+pug_html = pug_html + "\u003Cdiv class=\"entry\"\u003E\u003Cdiv class=\"time\"\u003E" + (pug_escape(null == (pug_interp = formatTime(new Date(entry.created_at))) ? "" : pug_interp)) + "\u003C\u002Fdiv\u003E\u003Cpre class=\"content\"\u003E" + (pug_escape(null == (pug_interp = entry.content) ? "" : pug_interp)) + "\u003C\u002Fpre\u003E\u003C\u002Fdiv\u003E";
+      }
+  } else {
+    var $$l = 0;
+    for (var pug_index0 in $$obj) {
+      $$l++;
+      var entry = $$obj[pug_index0];
+pug_html = pug_html + "\u003Cdiv class=\"entry\"\u003E\u003Cdiv class=\"time\"\u003E" + (pug_escape(null == (pug_interp = formatTime(new Date(entry.created_at))) ? "" : pug_interp)) + "\u003C\u002Fdiv\u003E\u003Cpre class=\"content\"\u003E" + (pug_escape(null == (pug_interp = entry.content) ? "" : pug_interp)) + "\u003C\u002Fpre\u003E\u003C\u002Fdiv\u003E";
+    }
+  }
+}).call(this);
+}.call(this,"Date" in locals_for_with?locals_for_with.Date:typeof Date!=="undefined"?Date:undefined,"entries" in locals_for_with?locals_for_with.entries:typeof entries!=="undefined"?entries:undefined,"formatTime" in locals_for_with?locals_for_with.formatTime:typeof formatTime!=="undefined"?formatTime:undefined));;return pug_html;}
